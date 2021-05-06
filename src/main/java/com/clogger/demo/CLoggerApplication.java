b147c22e95ae3bdcbef4a08ef4df915ec1474869
aR chaudhary <achintyac77@gmail.com>
2021-05-06 21:39:59 +0530

    initial setup

---
+++ src/main/java/com/clogger/demo/CLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.clogger.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CLoggerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clogger/demo/CLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clogger.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
